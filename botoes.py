a8ce0adee6399412f40cb458bd7e97eec9f88d73
diegodvsmelo <118706124+diegodvsmelo@users.noreply.github.com>
2024-03-13 21:16:13 -0300

    após clicar para comprar torre, aparecerá um botão de cancelar a compra, e as torres agora não pode ser colocadas na barra lateral do jogo, apenas nas coordenadas dentro do mapa

--- botoes.py
+++ botoes.py
@@ -1,11 +1,12 @@
 import pygame as pg 
 
 class Botão():
-    def __init__(self, x, y, imagem):
+    def __init__(self, x, y, imagem, single_click):
         self.image = imagem
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.clicked = False
+        self.single_click = single_click
     
     def draw(self, surface):
         acao = False
@@ -16,7 +17,9 @@ class Botão():
         if self.rect.collidepoint(pos):
             if pg.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 acao = True
-                self.clicked = True
+                #se o botão desejado for single click, então 'clicked' é verdadeiro
+                if self.single_click:
+                    self.clicked = True
         #o [0] significa o botão esquerdo do mouse, e o == 0 significa que ele não esta sendo apertado, == 1 significa q ele foi apertado
         if pg.mouse.get_pressed()[0] == 0:
             self.clicked = False
--- main.py
+++ main.py
@@ -16,11 +16,13 @@ win = pg.display.set_mode((SCREEN_HEIGHT + SIDE_BAR, SCREEN_WIDTH))
 pg.display.set_caption("Projeto P1")
 
 # carregando imagens
-imagem_mapa = pg.image.load('imagens/mapa.png').convert_alpha()
-image_side = pg.image.load('imagens/madeira.jpg').convert_alpha()
-imagem_inimigo = pg.image.load('imagens/inimigo.png').convert_alpha()
-imagem_compra_torre = pg.image.load('imagens/botão_torre.png').convert_alpha()
-imagem_torre = pg.image.load('imagens/torretas1_1.png').convert_alpha()
+imagem_mapa = pg.image.load('imagens/niveis/mapa.png').convert_alpha()
+image_side = pg.image.load('imagens/hud/madeira.jpg').convert_alpha()
+imagem_inimigo = pg.image.load('imagens/inimigo_1/inimigo.png').convert_alpha()
+imagem_compra_torre = pg.image.load('imagens/hud/botao_torre.png').convert_alpha()
+imagem_cancelar_compra = pg.image.load('imagens/hud/botao_cancelar_compra.png').convert_alpha()
+imagem_torre = pg.image.load('imagens/torre_1/torretas1_1.png').convert_alpha()
+torre_cursor = pg.image.load('imagens/torre_1/torretas1_1.png').convert_alpha()
 
 # mundo
 mundo = Mapa(imagem_mapa)
@@ -33,7 +35,8 @@ grupo_inimigos.add(inimigo)
 grupo_torres = pg.sprite.Group()  # Grupo para torres
 
 # criação dos botões
-compra_torre = Botão(SCREEN_HEIGHT + 30, 100, imagem_compra_torre)
+compra_torre = Botão(SCREEN_HEIGHT + 30, 100, imagem_compra_torre, True)
+cancelar = Botão(SCREEN_HEIGHT+ 30, 200, imagem_cancelar_compra, True)
 
 # Loop principal do jogo
 run = True
@@ -42,33 +45,38 @@ while run:
     
     clock.tick(FPS)
     
+    if aguardando_posicao_torre == True:
+        #mostrar torre no cursor:
+        
+        if cancelar.draw(win):
+            aguardando_posicao_torre = False
     # Verifica eventos
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
-        elif event.type == pg.MOUSEBUTTONDOWN: 
-            if compra_torre.rect.collidepoint(event.pos):  
+        if event.type == pg.MOUSEBUTTONDOWN: 
+            mouse_pos = pg.mouse.get_pos()
+            if event.button == 1 and compra_torre.rect.collidepoint(event.pos):  
                 print('Clique no botão de compra de torre')
                 # Define uma variável de controle para a criação da torre no próximo clique do botão direito
                 aguardando_posicao_torre = True
-            elif event.button == 3 and aguardando_posicao_torre: 
-                print('Clique com o botão direito')
-                nova_torre = Torre(imagem_torre, event.pos) 
-                grupo_torres.add(nova_torre) 
-                aguardando_posicao_torre = False 
-
+            if mouse_pos[0] < SCREEN_HEIGHT and mouse_pos[1] < SCREEN_WIDTH:
+                if event.button == 3 and aguardando_posicao_torre == True: 
+                    print('Clique com o botão direito')
+                    nova_torre = Torre(imagem_torre, event.pos) 
+                    grupo_torres.add(nova_torre) 
+                    aguardando_posicao_torre = False 
     # Atualizações
     grupo_inimigos.update()
     grupo_torres.update()
-
+    pg.display.update()
     # Desenhos
-    win.fill((0, 0, 0))  # Preenche a tela com a cor preta
     mundo.draw(win)
-    sidebar.draw(win)
+    if aguardando_posicao_torre == False:
+        sidebar.draw(win)
     grupo_inimigos.draw(win)
     grupo_torres.draw(win)
     compra_torre.draw(win)
 
     pg.display.update()
-
 pg.quit()
